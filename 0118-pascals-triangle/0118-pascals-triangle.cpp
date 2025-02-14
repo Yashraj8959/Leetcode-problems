6e1bc074aa29ed4c4a80d3f50a15611d3c138a32
Yashraj kushwah <110288913+Yashraj8959@users.noreply.github.com>
2025-02-14 13:15:50 +0530

    Time: 0 ms (100%), Space: 9.8 MB (12.65%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> temp(numRows);
+        for(int i= 0; i<numRows; i++){
+            temp[i].resize(i+1,1);
+            for(int j = 1; j<i; j++){
+                temp[i][j] = temp[i-1][j-1] + temp[i-1][j];
+            }
+        }
+
+    return temp;
+    }
+};
