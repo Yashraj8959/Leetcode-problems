1f6d14762af6eddef849240c0e91f3b789eca80b
Yashraj kushwah <110288913+Yashraj8959@users.noreply.github.com>
2025-02-20 12:13:11 +0530

    Time: 3 ms (20.91%), Space: 9.3 MB (61.6%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> map;
+        int oddCount = 0;
+        for( auto ch : s){
+            map[ch]++;
+            
+            if(map[ch]%2==1)
+                oddCount++;
+            else oddCount--; 
+        
+        }
+        if (oddCount > 1)
+        return s.length() - oddCount +1;
+        return s.length();
+    }
+};
